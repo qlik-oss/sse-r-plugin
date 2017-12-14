249690e9445898a8e8a597dbda35324b56ce8051
Tobias Lindulf <tobias.lindulf@qlik.com>
2017-12-14 23:56:11 +0100

    Updated grpc lib to 1.7.3 and protobuf lib to 3.5.0

--- csharp/ServerSideExtension/generate_SSE_Stubs_from_proto.bat
+++ csharp/ServerSideExtension/generate_SSE_Stubs_from_proto.bat
@@ -3,7 +3,7 @@ IF %1.==. GOTO NoArg
 cd %1
 :NoArg
 @echo on
-set TOOLS_DIR="..\packages\Grpc.Tools.1.2.2\tools\windows_x64"
+set TOOLS_DIR="..\packages\Grpc.Tools.1.7.3\tools\windows_x64"
 set PROTO_DIR="..\..\proto"
 %TOOLS_DIR%\protoc -I%PROTO_DIR% --grpc_out=. --csharp_out=. --plugin=protoc-gen-grpc=%TOOLS_DIR%\grpc_csharp_plugin.exe %PROTO_DIR%\ServerSideExtension.proto
 exit /b %ERRORLEVEL%
